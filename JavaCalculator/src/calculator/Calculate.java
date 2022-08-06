e09b630f693521ae20fb1bf2c4237b398298eae5
Becky Shepherd <2109364s@student.gla.ac.uk>
2022-08-06 15:05:24 +0100

    minor changes to increase functionality

--- JavaCalculator/src/calculator/Calculate.java
+++ JavaCalculator/src/calculator/Calculate.java
@@ -4,19 +4,20 @@ public class Calculate {
 	private static float answer;
 	
 	public static float calculate(String[] userCalculation) {
-		try{
-			float val1 = Integer.parseInt(userCalculation[0]);
+		if(!userCalculation[0].equals("clear")) {
 			for(int calculationIndex = 1; calculationIndex<userCalculation.length; calculationIndex += 2) {
 				String val2 = userCalculation[calculationIndex + 1];
 				String op = userCalculation[calculationIndex];
-				if(calculationIndex == 1) {
+				if(calculationIndex == 1 && answer == 0) {
+					float val1 = Integer.parseInt(userCalculation[calculationIndex - 1]);
 					answer = OperatorConversion.operationApplication(val1, op, val2);
 				}
 				else {
 					answer = OperatorConversion.operationApplication(answer, op, val2);
 				}
 			}
-		} catch(Exception e){
+		}
+		else {
 			answer = 0;
 		}
 		return answer;	
--- JavaCalculator/src/calculator/UserInteration.java
+++ JavaCalculator/src/calculator/UserInteration.java
@@ -1,27 +1,22 @@
 package calculator;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class UserInteration {
-	static float finalAnswer;
+	static float finalAnswer = 0.0f;
 	
 	private static void calculator(){
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("Please enter your calculation with section seperated by a space.\n" +
                   "When ready to calculate please press enter. Example: 2 + 2 * 10 - 6\n" + 
                   "Your previous answer will be stored. if you wish to start again at 0 please enter clear\n" + 
+                  "If you wish to start with the previous answer simply enter ans instead of an integer\n" +
                   "When you wish to exit please enter q.");
 		String newInput = userInput.nextLine().replace("\n", "");
 		newInput = newInput.replace("#", "");
 		while(!newInput.equals("q")) {
 			String[] userCalculation = newInput.split(" ");
-			if(userCalculation[0].equals("clear")) {
-				finalAnswer = 0;
-			}
-			else {
-				finalAnswer += Calculate.calculate(userCalculation);
-			}
+			finalAnswer = Calculate.calculate(userCalculation);
 			System.out.println(finalAnswer);
 			newInput = userInput.nextLine();
 		}
